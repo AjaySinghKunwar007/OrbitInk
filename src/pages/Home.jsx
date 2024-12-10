5c1c73da8464d5e89f20484e10c0ee36ba739f19
Ajay Singh Kunwar <ajaykunwar323@gmail.com>
2024-12-10 13:18:43 +0530

    added : loader utility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import databaseService from "../appwrite/database";
 import { Container, PostCard } from "../components/index";
+import Loader from "../utils/Loader";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -29,18 +30,7 @@ function Home() {
 
   if (loading) {
     return (
-      <div className=" h-screen w-full">
-      <Container>
-        <div className="flex flex-col items-center justify-center h-[80vh] w-full">
-          <div
-            className="w-32 h-32 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"
-            role="status"
-            aria-live="polite"
-          ></div>
-          <p className="mt-2 text-lg text-gray-600">Loading post...</p>
-        </div>
-      </Container>
-    </div>
+      <Loader/>
     );
   }
 
@@ -56,17 +46,7 @@ function Home() {
     );
   }
 
-  // if (posts.length === 0) {
-  //   return (
-  //     <div className="w-full py-8 mt-4 text-center">
-  //       <Container>
-  //         <h1 className="text-2xl font-bold text-gray-700">
-  //           Login to read posts
-  //         </h1>
-  //       </Container>
-  //     </div>
-  //   );
-  // }
+
   if(posts.length>0)  {
      return <div className="flex flex-wrap justify-center">
     
---
+++ src/utils/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from '../components'
+
+function Loader() {
+  return (
+    <div className=" h-screen w-full">
+            <Container>
+              <div className="flex flex-col items-center justify-center h-[80vh] w-full">
+                <div
+                  className="w-32 h-32 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"
+                  role="status"
+                  aria-live="polite"
+                ></div>
+                <p className="mt-2 text-lg text-gray-600">Loading post...</p>
+              </div>
+            </Container>
+          </div>
+  );
+}
+
+export default Loader
